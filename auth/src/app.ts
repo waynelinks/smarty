f7d4093d300e9a1f3b94f21ddffef41df6ce0113
Wayne Links <wayne.links@gmail.com>
2020-10-26 22:25:15 +0200

    build(middleware): setup common middleware

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,6 +1,12 @@
 import 'express-async-errors'
 import express from 'express'
+import { middleware, notFound, errorHandler } from '@bigoncloud/middleware'
 
 const app = express()
+
+middleware(app, express)
+
+notFound(app)
+app.use(errorHandler)
 
 export { app }
