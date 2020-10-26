5401f009a74c2576e23d7000e27060aae6b0f283
Wayne Links <wayne.links@gmail.com>
2020-10-26 22:30:12 +0200

    build(token): create token interface

---
+++ auth/src/interfaces/ItokenPayload.ts
@@ -0,0 +1,9 @@
+export interface TokenPayload {
+  aud: string
+  sub: string
+  iss: string
+  iat: number
+  exp: number
+  name: string
+  email: string
+}
--- auth/src/interfaces/index.ts
+++ auth/src/interfaces/index.ts
@@ -1,1 +1,2 @@
+export * from './ItokenPayload'
 export * from './IUser'
