2899dd84a0bfe5405635ced1ee678715c7c0b968
Wayne Links <wayne.links@gmail.com>
2020-10-27 00:26:28 +0200

    build(api): create current user controller

--- auth/src/routes/v1/index.ts
+++ auth/src/routes/v1/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
+import { currentUser } from '@bigoncloud/middleware'
 
 import { validate } from '../../middleware'
-import { register, signin, signout } from '../../controllers'
+import { currentActiveUser, register, signin, signout } from '../../controllers'
 
 const router = Router()
 
 router.route('/register').post(validate('register'), register)
 router.route('/signin').post(validate('signin'), signin)
 router.route('/signout').post(signout)
+router.route('/currentuser').get(currentUser, currentActiveUser)
 
 export { router as routes }
