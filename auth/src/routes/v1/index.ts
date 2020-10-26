28489e5d33181c161713bf56768e973f2bfe2f79
Wayne Links <wayne.links@gmail.com>
2020-10-27 00:23:54 +0200

    build(api): create signout controller

--- auth/src/routes/v1/index.ts
+++ auth/src/routes/v1/index.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 
 import { validate } from '../../middleware'
-import { register, signin } from '../../controllers'
+import { register, signin, signout } from '../../controllers'
 
 const router = Router()
 
 router.route('/register').post(validate('register'), register)
 router.route('/signin').post(validate('signin'), signin)
+router.route('/signout').post(signout)
 
 export { router as routes }
